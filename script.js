dfdd6c7e45fdd81c267758c83ab5d924977ca2f9
Martosky <imoniaromartins@yahoo.com>
2024-01-20 22:45:08 +0100

    app now functioning

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let slideIndex = 1;
+slideModal(slideIndex);
+
+function addSlide(n){
+    slideModal(slideIndex += n)
+}
+ function currentSlide(n){
+    slideModal(slideIndex = n)
+ }
 
 function slideModal(n){
-    const slide = document.getElementsByClassName("mySlides");
-    const dots = document.getElementsByClassName("dot");
-    if( n > slide.length){slideIndex = 1};
+    let slide = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if(n > slide.length){slideIndex = 1};
     if (n < 1){slideIndex = slide.length}
 
     for (let i = 0; i < slide.length; i++){
-        slide[i].computedStyleMap.display = "none";
+        slide[i].style.display = "none";
     }
-    for (let i = 0; i < dots.length){
-        dots[i].className = dots[i].className.replace(" active", "")
+    for (let i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");
     }
+    slide[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
