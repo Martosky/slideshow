c36acf0c34641d0b9b94107c3715a8022e041579
Martosky <imoniaromartins@yahoo.com>
2024-01-20 21:46:46 +0100

    added more functionalities to my app

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+let slideIndex = 1;
+
 function slideModal(n){
     const slide = document.getElementsByClassName("mySlides");
     const dots = document.getElementsByClassName("dot");
+    if( n > slide.length){slideIndex = 1};
+    if (n < 1){slideIndex = slide.length}
+
+    for (let i = 0; i < slide.length; i++){
+        slide[i].computedStyleMap.display = "none";
+    }
+    for (let i = 0; i < dots.length){
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
 }
